9f8b03f48ee7085f4343dbc279ecd20e4f53c4de
mydreamc <1476650484@qq.com>
2020-06-20 00:52:11 +0800

    one:old-unix-c

---
+++ math/calc.h
@@ -0,0 +1,4 @@
+#ifndef __CALC_H
+#define __CALC_H
+double add(double, double);
+#endif// __CALC.H__
---
+++ math/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "calc.h"
+
+int main(void){
+	double val = 3.8;
+	double num = 5.2;
+	printf("%lf\n", add(val, num));
+	return 0;
+}
